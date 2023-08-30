2f0d789ade18ec6850e3a4ff0f0376a02436b51b
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2023-08-30 23:34:12 +0100

    commit on macbook

---
+++ src/TrialClass.java
@@ -0,0 +1,15 @@
+public class TrialClass {
+
+    private int age;
+    private String name;
+
+    public TrialClass(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+    public int ageAddedFive() {
+        return this.age + 5;
+    }
+}
